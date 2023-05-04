ce76ae3010157a7287b836ea66c87b7f02ca028c
NusratMitu <nusratmitu010@.com>
2023-05-04 16:59:25 +0600

    new

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+const about = () => {
+  return (
+    <div>
+        <Head>
+
+        <title> Next App About</title>
+        </Head>
+        <div>
+            <h1>about</h1>
+        </div>
+    </div>
+  )
+}
+
+export default about
