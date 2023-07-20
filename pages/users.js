a49ddf9f3031468759dfede2506571cf76b253f3
NusratMitu <nusratmitu010@.com>
2023-07-20 16:42:16 +0600

    rendering SSG

---
+++ pages/users.js
@@ -0,0 +1,31 @@
+import User from "@/components/user";
+
+const UserList = ({users}) => {
+    // console.log({users});
+  return (
+    <>
+        
+        <h1>list of users </h1>
+        {
+            users.map((user) =>
+            <div key={user.id}>
+<User user={user}/>
+            </div>
+            )
+        }
+    </>
+  )
+}
+
+export default UserList;
+
+export async function getStaticProps() {
+const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+const data = await response.json();
+// console.log(data); 
+ return {
+    props : {
+        users: data
+    }
+ }
+}
